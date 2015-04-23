8053b6903db6f50bdd184ebcfb616850264fdd01
manudhundi <manu@couchbase.com>
2015-04-23 16:31:54 -0700

    Add XDCR test

--- perfrunner/tests/dcp.py
+++ perfrunner/tests/dcp.py
@@ -45,3 +45,50 @@ class ViewTest(IndexTest):
         self.workload = self.test_config.access_settings
         self.access_bg()
 	self.access() 
+	
+class XdcrTest(PerfTest):
+
+    COLLECTORS = {'replicate_latency': True}
+
+    def __init__(self, *args):
+        super(XdcrTest, self).__init__(*args)
+
+    @with_stats
+    def access(self):
+        super(XdcrTest, self).timer()
+
+    def _start_replication(self, m1, m2):
+        name = target_hash(m1, m2)
+        certificate = self.settings.use_ssl and self.rest.get_certificate(m2)
+        self.rest.add_remote_cluster(m1, m2, name, certificate)
+
+        for bucket in self.test_config.buckets:
+            params = {
+                'replicationType': 'continuous',
+                'toBucket': bucket,
+                'fromBucket': bucket,
+                'toCluster': name
+            }
+            if self.settings.replication_protocol:
+                params['type'] = self.settings.replication_protocol
+            self.rest.start_replication(m1, params)
+
+    def enable_xdcr(self):
+        m1, m2 = self.cluster_spec.yield_masters()
+
+        if self.settings.replication_type == 'unidir':
+            self._start_replication(m1, m2)
+        if self.settings.replication_type == 'bidir':
+            self._start_replication(m1, m2)
+            self._start_replication(m2, m1)
+
+    def run(self):
+        self.load()
+        self.wait_for_persistence()
+
+        self.enable_xdcr()
+        self.compact_bucket()
+
+        self.workload = self.test_config.access_settings
+        self.access_bg()
+	self.access() 

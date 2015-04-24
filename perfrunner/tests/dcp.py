77b05fdc2f05ffabe5697b5e5109fb0fa6534cbf
manudhundi <manu@couchbase.com>
2015-04-23 17:41:59 -0700

    call xdcr functions by importing them

--- perfrunner/tests/dcp.py
+++ perfrunner/tests/dcp.py
@@ -3,6 +3,7 @@ from perfrunner.tests import PerfTest
 from perfrunner.helpers.cbmonitor import with_stats
 from perfrunner.workloads.viewgen import ViewGen
 from perfrunner.tests.index import IndexTest
+from perfrunner.tests.xdcr import XdcrTest
 
 class ReplicationTest(PerfTest):
 
@@ -46,7 +47,7 @@ class ViewTest(IndexTest):
         self.access_bg()
 	self.access() 
 	
-class XdcrTest(PerfTest):
+class DcpXdcrTest(XdcrTest):
 
     COLLECTORS = {'replicate_latency': True}
 
@@ -56,31 +57,6 @@ class XdcrTest(PerfTest):
     @with_stats
     def access(self):
         super(XdcrTest, self).timer()
-
-    def _start_replication(self, m1, m2):
-        name = target_hash(m1, m2)
-        certificate = self.settings.use_ssl and self.rest.get_certificate(m2)
-        self.rest.add_remote_cluster(m1, m2, name, certificate)
-
-        for bucket in self.test_config.buckets:
-            params = {
-                'replicationType': 'continuous',
-                'toBucket': bucket,
-                'fromBucket': bucket,
-                'toCluster': name
-            }
-            if self.settings.replication_protocol:
-                params['type'] = self.settings.replication_protocol
-            self.rest.start_replication(m1, params)
-
-    def enable_xdcr(self):
-        m1, m2 = self.cluster_spec.yield_masters()
-
-        if self.settings.replication_type == 'unidir':
-            self._start_replication(m1, m2)
-        if self.settings.replication_type == 'bidir':
-            self._start_replication(m1, m2)
-            self._start_replication(m2, m1)
 
     def run(self):
         self.load()
